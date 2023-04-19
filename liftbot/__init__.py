7e35c66839ab6ecba997b4ae9cf41fea1c0536e1
drmr <126770071+tv-drmr@users.noreply.github.com>
2023-04-19 21:41:26 +0200

    d

--- liftbot/__init__.py
+++ liftbot/__init__.py
@@ -15,15 +15,15 @@ async def on_ready():
     print(Center.XCenter(Colorate.Horizontal(Colors.red_to_yellow, f"""
 
 
-                        ██╗     ██╗███████╗████████╗ ██████╗ ██████╗ ██████╗ ██████╗ 
-                        ██║     ██║██╔════╝╚══██╔══╝██╔════╝██╔═══██╗██╔══██╗██╔══██╗
-                        ██║     ██║█████╗     ██║   ██║     ██║   ██║██████╔╝██║  ██║
-                        ██║     ██║██╔══╝     ██║   ██║     ██║   ██║██╔══██╗██║  ██║
-                        ███████╗██║██║        ██║   ╚██████╗╚██████╔╝██║  ██║██████╔╝
-                        ╚══════╝╚═╝╚═╝        ╚═╝    ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ 
+                        ██╗     ██╗███████╗████████╗██████╗  ██████╗ ████████╗
+                        ██║     ██║██╔════╝╚══██╔══╝██╔══██╗██╔═══██╗╚══██╔══╝
+                        ██║     ██║█████╗     ██║   ██████╔╝██║   ██║   ██║   
+                        ██║     ██║██╔══╝     ██║   ██╔══██╗██║   ██║   ██║   
+                        ███████╗██║██║        ██║   ██████╔╝╚██████╔╝   ██║   
+                        ╚══════╝╚═╝╚═╝        ╚═╝   ╚═════╝  ╚═════╝    ╚═╝   
 
-                                                                            """)))
-    print(Center.XCenter(Colorate.Horizontal(Colors.red_to_yellow, f"Connected {bot.user.name}#{bot.user.discriminator} | Version: 12.1.5 | Time: {current_dateTime.hour}:{current_dateTime.minute}")))
+                                                      """)))
+    print(Center.XCenter(Colorate.Horizontal(Colors.red_to_yellow, f"Connected {bot.user.name}#{bot.user.discriminator} | Version: 12.1.6 | Local Time: {current_dateTime.hour}:{current_dateTime.minute}")))
 
 
 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ packages = [
 
 setup(
     name="liftbot.py selfbot",
-    version=f"12.1.5",
+    version=f"12.1.6",
     packages=packages,
     include_package_data=True,
     license="MIT License",
