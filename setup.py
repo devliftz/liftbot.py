e3f511600805e92f89d65739b912bc334e6e23ae
drmr <126770071+tv-drmr@users.noreply.github.com>
2023-04-19 21:39:20 +0200

    d

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ packages = [
 
 setup(
     name="liftbot.py selfbot",
-    version=f"12.1.1",
+    version=f"12.1.5",
     packages=packages,
     include_package_data=True,
     license="MIT License",
