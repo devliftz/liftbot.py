79103e35f348cd80c6856c33bf8138cab8cec947
drmr <126770071+tv-drmr@users.noreply.github.com>
2023-04-19 21:35:35 +0200

    f

--- liftbot/__init__.py
+++ liftbot/__init__.py
@@ -5,13 +5,7 @@ from datetime import datetime
 from liftbot import gradient
 from liftbot.styler import Colorate, Colors, Center
 
-bot = commands.Bot(command_prefix="?", help_command=None, self_bot=True)
-
-@bot.event
-async def on_ready():
-    await bot.change_presence(activity=discord.Streaming(name=f"👻 | {bot.user.name}"))
-    current_dateTime = datetime.now()
-    print(Center.XCenter(Colorate.Horizontal(Colors.red_to_yellow, f"""
+print(Center.XCenter(Colorate.Horizontal(Colors.red_to_yellow, f"""
 
 
                     ██╗     ██╗███████╗████████╗ ██████╗ ██████╗ ██████╗ ██████╗ 
@@ -22,7 +16,15 @@ async def on_ready():
                     ╚══════╝╚═╝╚═╝        ╚═╝    ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ 
 
                                                                          """)))
-    print(Center.XCenter(Colorate.Horizontal(Colors.red_to_yellow, f"Connected {bot.user.name}#{bot.user.discriminator} | Version: 1.6.2 | Time: {current_dateTime.hour}:{current_dateTime.minute}")))
+print(Center.XCenter(Colorate.Horizontal(Colors.red_to_yellow, f"Connected {bot.user.name}#{bot.user.discriminator} | Version: 1.6.3 | Time: {current_dateTime.hour}:{current_dateTime.minute}")))
+
+bot = commands.Bot(command_prefix="?", help_command=None, self_bot=True)
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Streaming(name=f"👻 | {bot.user.name}"))
+    current_dateTime = datetime.now()
+
 
 @bot.event
 async def on_command(ctx):
